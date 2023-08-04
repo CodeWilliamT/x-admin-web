b05f79f772295a3564357440101055be529e12c5
Weiliang Tong <v-wetong@microsoft.com>
2023-08-04 10:05:45 +0000

    first commit

---
+++ src/views/test/test1.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>func1</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/test/test3.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>func3</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
